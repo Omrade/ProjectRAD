3bf8b648dfc14e353122bccb8e8d919a8db2fada
Omrade <omrade.shpagin@yandex.ru>
2022-06-25 00:14:23 +0700

    Start creating a program

---
+++ RAD.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace ProjectRAD {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ RAD
+	/// </summary>
+	public ref class RAD : public System::Windows::Forms::Form
+	{
+	public:
+		RAD(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~RAD()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// RAD
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(502, 331);
+			this->Name = L"RAD";
+			this->Text = L"RAD";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
