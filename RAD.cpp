8855ba6698e47be15fc0979049472539dfaa919c
Omrade <omrade.shpagin@yandex.ru>
2022-06-25 01:16:47 +0700

    Create Table

--- RAD.cpp
+++ RAD.cpp
@@ -1,2 +1,15 @@
 #include "RAD.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main(array<String^>^ arg)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	ProjectRAD::RAD form;
+	Application::Run(% form);
+}
+
--- RAD.h
+++ RAD.h
@@ -34,12 +34,49 @@ namespace ProjectRAD {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^ ôàéëToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ äåéñòâèÿToolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^ äîáàâèòüToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ îáíîâèòüToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ óäàëèòüToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ îÏğîãğàììåToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ âûõîäToolStripMenuItem;
+
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::Button^ button_delete;
+
+	private: System::Windows::Forms::Button^ button_change;
+
+
+
+
+	private: System::Windows::Forms::Button^ button_add;
+
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ name;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Shifr;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Denomination;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Quantity_pcs;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Notes;
+
+
+
+
+
+
+
+
+	protected:
+
+
+	private: System::ComponentModel::IContainer^ components;
 
 	private:
 		/// <summary>
 		/// Îáÿçàòåëüíàÿ ïåğåìåííàÿ êîíñòğóêòîğà.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,18 +85,194 @@ namespace ProjectRAD {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->ôàéëToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->äåéñòâèÿToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->äîáàâèòüToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->îáíîâèòüToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->óäàëèòüToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->îÏğîãğàììåToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->âûõîäToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->button_delete = (gcnew System::Windows::Forms::Button());
+			this->button_change = (gcnew System::Windows::Forms::Button());
+			this->button_add = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->name = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Shifr = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Denomination = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Quantity_pcs = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Notes = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->menuStrip1->SuspendLayout();
+			this->groupBox1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+				this->ôàéëToolStripMenuItem,
+					this->äåéñòâèÿToolStripMenuItem1, this->îÏğîãğàììåToolStripMenuItem, this->âûõîäToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(908, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// ôàéëToolStripMenuItem
+			// 
+			this->ôàéëToolStripMenuItem->Name = L"ôàéëToolStripMenuItem";
+			this->ôàéëToolStripMenuItem->Size = System::Drawing::Size(48, 20);
+			this->ôàéëToolStripMenuItem->Text = L"Ôàéë";
+			// 
+			// äåéñòâèÿToolStripMenuItem1
+			// 
+			this->äåéñòâèÿToolStripMenuItem1->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->äîáàâèòüToolStripMenuItem,
+					this->îáíîâèòüToolStripMenuItem, this->óäàëèòüToolStripMenuItem
+			});
+			this->äåéñòâèÿToolStripMenuItem1->Name = L"äåéñòâèÿToolStripMenuItem1";
+			this->äåéñòâèÿToolStripMenuItem1->Size = System::Drawing::Size(70, 20);
+			this->äåéñòâèÿToolStripMenuItem1->Text = L"Äåéñòâèÿ";
+			this->äåéñòâèÿToolStripMenuItem1->Click += gcnew System::EventHandler(this, &RAD::äåéñòâèÿToolStripMenuItem1_Click);
+			// 
+			// äîáàâèòüToolStripMenuItem
+			// 
+			this->äîáàâèòüToolStripMenuItem->Name = L"äîáàâèòüToolStripMenuItem";
+			this->äîáàâèòüToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->äîáàâèòüToolStripMenuItem->Text = L"Äîáàâèòü";
+			// 
+			// îáíîâèòüToolStripMenuItem
+			// 
+			this->îáíîâèòüToolStripMenuItem->Name = L"îáíîâèòüToolStripMenuItem";
+			this->îáíîâèòüToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->îáíîâèòüToolStripMenuItem->Text = L"Èçìåíèòü";
+			// 
+			// óäàëèòüToolStripMenuItem
+			// 
+			this->óäàëèòüToolStripMenuItem->Name = L"óäàëèòüToolStripMenuItem";
+			this->óäàëèòüToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->óäàëèòüToolStripMenuItem->Text = L"Óäàëèòü";
+			// 
+			// îÏğîãğàììåToolStripMenuItem
+			// 
+			this->îÏğîãğàììåToolStripMenuItem->Name = L"îÏğîãğàììåToolStripMenuItem";
+			this->îÏğîãğàììåToolStripMenuItem->Size = System::Drawing::Size(94, 20);
+			this->îÏğîãğàììåToolStripMenuItem->Text = L"Î ïğîãğàììå";
+			// 
+			// âûõîäToolStripMenuItem
+			// 
+			this->âûõîäToolStripMenuItem->Name = L"âûõîäToolStripMenuItem";
+			this->âûõîäToolStripMenuItem->Size = System::Drawing::Size(53, 20);
+			this->âûõîäToolStripMenuItem->Text = L"Âûõîä";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->button_delete);
+			this->groupBox1->Controls->Add(this->button_change);
+			this->groupBox1->Controls->Add(this->button_add);
+			this->groupBox1->Location = System::Drawing::Point(12, 27);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(88, 314);
+			this->groupBox1->TabIndex = 2;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Äåéñòâèÿ";
+			this->groupBox1->Enter += gcnew System::EventHandler(this, &RAD::groupBox1_Enter);
+			// 
+			// button_delete
+			// 
+			this->button_delete->Location = System::Drawing::Point(7, 77);
+			this->button_delete->Name = L"button_delete";
+			this->button_delete->Size = System::Drawing::Size(75, 23);
+			this->button_delete->TabIndex = 2;
+			this->button_delete->Text = L"Óäàëèòü";
+			this->button_delete->UseVisualStyleBackColor = true;
+			// 
+			// button_change
+			// 
+			this->button_change->Location = System::Drawing::Point(7, 48);
+			this->button_change->Name = L"button_change";
+			this->button_change->Size = System::Drawing::Size(75, 23);
+			this->button_change->TabIndex = 1;
+			this->button_change->Text = L"Èçìåíèòü";
+			this->button_change->UseVisualStyleBackColor = true;
+			// 
+			// button_add
+			// 
+			this->button_add->Location = System::Drawing::Point(6, 19);
+			this->button_add->Name = L"button_add";
+			this->button_add->Size = System::Drawing::Size(75, 23);
+			this->button_add->TabIndex = 0;
+			this->button_add->Text = L"Äîáàâèòü";
+			this->button_add->UseVisualStyleBackColor = true;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(5) {
+				this->name, this->Shifr,
+					this->Denomination, this->Quantity_pcs, this->Notes
+			});
+			this->dataGridView1->Location = System::Drawing::Point(106, 27);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(790, 302);
+			this->dataGridView1->TabIndex = 1;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &RAD::dataGridView1_CellContentClick);
+			// 
+			// name
+			// 
+			this->name->HeaderText = L"Íàèìåíîâàíèå";
+			this->name->Name = L"name";
+			// 
+			// Shifr
+			// 
+			this->Shifr->HeaderText = L"Øèôğ";
+			this->Shifr->Name = L"Shifr";
+			// 
+			// Denomination
+			// 
+			this->Denomination->HeaderText = L"Íîìèíàë";
+			this->Denomination->Name = L"Denomination";
+			// 
+			// Quantity_pcs
+			// 
+			this->Quantity_pcs->HeaderText = L"Êîëè÷åñòâî, øò";
+			this->Quantity_pcs->Name = L"Quantity_pcs";
+			// 
+			// Notes
+			// 
+			this->Notes->HeaderText = L"Ïğèìå÷àíèÿ";
+			this->Notes->Name = L"Notes";
 			// 
 			// RAD
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(502, 331);
+			this->ClientSize = System::Drawing::Size(908, 341);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"RAD";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"RAD";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->groupBox1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void äåéñòâèÿToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void äåéñòâèÿToolStripMenuItem1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void groupBox1_Enter(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
+};
 }
